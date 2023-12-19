98cc829e2354de678dc0062d47f25be9a132a736
anuhari121 <132882834+anuhari121@users.noreply.github.com>
2023-12-20 00:44:50 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,4 +6,8 @@ def mavenBuild()
 {
    sh 'mvn package'
 }
+def deployTomcat(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
   
